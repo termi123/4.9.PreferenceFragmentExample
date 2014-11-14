6b8ef233f084f7db9cb77168737b260a82c2e7ff
termi123 <dung.phantu@gmail.com>
2014-11-14 14:37:51 +0700

    create

---
+++ gen/net/learn2develop/PreferenceFragmentExample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package net.learn2develop.PreferenceFragmentExample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
